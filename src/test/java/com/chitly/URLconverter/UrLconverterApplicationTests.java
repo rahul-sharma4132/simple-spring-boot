64eb4523fc9284f6edeeaddc93ff083ab958ca8d
Sanjay Sharma <sanjaysharma@Sanjays-MacBook-Air.local>
2023-01-04 23:18:44 +0530

    Setup and base code

---
+++ src/test/java/com/chitly/URLconverter/UrLconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chitly.URLconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrLconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
