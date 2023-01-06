cadbaf411dcb98ab6279a1d9e62e835d8182b38d
Sanjay Sharma <sanjaysharma@Sanjays-MacBook-Air.local>
2023-01-06 11:21:35 +0530

    Removed useless files

--- src/main/java/com/chitly/URLconverter/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.chitly.URLconverter;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
