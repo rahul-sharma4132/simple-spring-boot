74b178f238cd840a5eceda9dc2de68e4ba8fcf15
Sanjay Sharma <sanjaysharma@Sanjays-MacBook-Air.local>
2023-01-06 11:03:04 +0530

    Cleaned up and made repo working

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,1 @@
-INSERT INTO User (id, name, email) VALUES (1, 'Adi','adi@gmail.com');
-INSERT INTO User (id, name) VALUES (2, 'Rahul','rahul@gmail.com');
-INSERT INTO User (id, name) VALUES (3, 'Akanksha','akanksha@gmail.com');
+
--- target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO User (id, name, email) VALUES (1, 'Adi','adi@gmail.com');
-INSERT INTO User (id, name) VALUES (2, 'Rahul','rahul@gmail.com');
-INSERT INTO User (id, name) VALUES (3, 'Akanksha','akanksha@gmail.com');
